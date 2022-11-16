680f6b3574b46998ef124a28feef250e9bef9bbc
Discoveren <renniel.pena@gmail.com>
2022-11-16 17:25:57 +0000

    Added average var and console log to display amount

--- my_file.js
+++ my_file.js
@@ -1,12 +1,17 @@
+//setting the integer to be the first
 var finalSum = 0;
+
+// for loop for the main and nested index.
 for (var main_index = 0; main_index < finances.length; main_index++ ) {
     for(var nested_index = 0; nested_index < finances[main_index].length; nested_index++) {
     }
-    // total variable for the sum of all numbers.
+    // total variable for the sum of all the profit.
     var total = finalSum += (finances[main_index][1])
+    var average = total / finances.length - 1
   
 
     
 }
 console.log('Total months: ' + main_index)
 console.log('Total: ' + '$' + (total - 1))
+console.log('Average:' + 'S' + Math.round(average))
