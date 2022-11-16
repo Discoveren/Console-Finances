323f7b86f85a4559bf45d1476a2681f68e0db578
Discoveren <renniel.pena@gmail.com>
2022-11-16 17:13:50 +0000

    added total variable and console log total

--- my_file.js
+++ my_file.js
@@ -1,5 +1,12 @@
+var finalSum = 0;
 for (var main_index = 0; main_index < finances.length; main_index++ ) {
     for(var nested_index = 0; nested_index < finances[main_index].length; nested_index++) {
-      console.log(finances[main_index][nested_index]);
     }
-  }
+    // total variable for the sum of all numbers.
+    var total = finalSum += (finances[main_index][1])
+  
+
+    
+}
+console.log('Total months: ' + main_index)
+console.log('Total: ' + '$' + (total - 1))
